6765225879db8c346325b11b8d05e609e4015f2e
Anton <whiletablesits@gmail.com>
2021-11-03 15:19:17 +0200

    API

---
+++ features/apiCurl.feature
@@ -0,0 +1,8 @@
+Feature: Api test
+
+  Scenario:
+    When request \\POST api/registration/add\\
+    Then request \\POST api/registration/type\\
+    Then request \\POST api/registration/role/bro\\
+    Then request \\GET api/login\\
+
---
+++ http-client.php
@@ -0,0 +1,36 @@
+<?php
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+
+class SymfonyDocs
+{
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+
+    public function fetchGitHubInformation(): array
+    {
+        $client = new \HttpClientInterface;
+        $response = $client->request(
+            'GET',
+            'https://api.github.com/repos/symfony/symfony-docs'
+        );
+
+        $statusCode = $response->getStatusCode();
+        // $statusCode = 200
+        $contentType = $response->getHeaders()['content-type'][0];
+        // $contentType = 'application/json'
+        $content = $response->getContent();
+        // $content = '{"id":521583, "name":"symfony-docs", ...}'
+        $content = $response->toArray();
+        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+
+        return $content;
+    }
+
+}
+
